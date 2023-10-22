dce6fd5c677cc532078cbd6bd8eac9172e017e0a
alejandroileal <aleal1202@outlook.com>
2023-10-22 16:39:18 +0200

    Build main function in app module

--- scripts/app.js
+++ scripts/app.js
@@ -9,19 +9,33 @@ import { generateModifiedCellTable } from './generateModifiedCellTable.js';
 import { generateNewCellTable } from './generateNewCellTable.js';
 import { printCellsTable } from './printCellsTable.js';
 
-let firstroundSwitch = true;
-const finalRoundTimer = 30;
-let currentTable;
+const runLifeGame = () => {
+  let firstroundSwitch = true;
+  const finalRoundTimer = 10;
+  let currentTable;
+  let counter;
+  counter = 0;
 
-if (firstroundSwitch === true) {
-  currentTable = generateNewCellTable(3, 3);
-  printCellsTable(currentTable);
-  firstroundSwitch = false;
-}
+  const newLifeGameTurn = () => {
+    if (counter <= finalRoundTimer) {
+      counter++;
 
-if (firstroundSwitch === false) {
-  for (let i = 0; i < finalRoundTimer; i++) {
-    currentTable = generateModifiedCellTable(currentTable);
-    printCellsTable(currentTable);
-  }
-}
+      if (firstroundSwitch === true) {
+        currentTable = generateNewCellTable(3, 3);
+        printCellsTable(currentTable);
+        firstroundSwitch = false;
+        setTimeout(newLifeGameTurn, 2000);
+      }
+
+      if (firstroundSwitch === false && counter > 1) {
+        currentTable = generateModifiedCellTable(currentTable);
+        printCellsTable(currentTable);
+        setTimeout(newLifeGameTurn, 2000);
+      }
+    }
+  };
+
+  newLifeGameTurn();
+};
+
+runLifeGame();
